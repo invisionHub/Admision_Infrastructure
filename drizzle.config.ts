a7dfc100c3336c85da19b2ad720552d38a01795b
kay2-dev <odufotekayode4@gmail.com>
2026-04-14 01:19:01 +0100

    route-setup: exam routes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "drizzle-kit";
 
 export default {
     schema: "./backend/db/schema.ts",
-    out: "./drizzle",
+    out: "./backend/drizzle",
     dialect: "postgresql",
     dbCredentials: {
         url: "postgresql://postgres.mrwigfjcofqtnrcvgaap:Password321invision@aws-1-eu-north-1.pooler.supabase.com:6543/postgres",
